d1508b796a2f65e9ee20986ec4039dd57d9ce4a3
kmcelwee <kevinrmcelwee@gmail.com>
2019-09-13 10:47:59 -0400

    box updates

--- js/main.js
+++ js/main.js
@@ -1,21 +1,34 @@
 $(document).ready(function() {
-	$('#filter li').click(function() {
+	var blurbs = $('.outer_blurb');
+	var hr = $('.hr');
+	var flexes = $('.flex_container');
 
+	$('#filter li').click(function() {
+		blurbs.css({'display': 'none'});
+		hr.css({'display': 'none'});
+		
 		$('.filter_active').toggleClass('filter_active');
 		$(this).toggleClass('filter_active');
 		
 		var filter = $(this).attr('id');
 		var tagged = $(`[tags *= "${filter}"]`);
 
-		if (filter == 'show_all') { tagged = $('.flex_container'); } 
+		if (filter == 'show_all') { tagged = flexes; } 
 		
-		$.when(
-			$('.flex_container').fadeOut("fast")
-		).then(function() {
-			tagged.delay(100).fadeIn(250);
-	   		
-		});
+		flexes.hide()
+		tagged.show()
 	});
+
+	blurbs.css({'display': 'none'});
+	hr.css({'display': 'none'});
+
+	$('.box').click(function() {
+		blurbs.show();
+		hr.show();
+		var where_to = '#' + this.id + '_flex_container';
+		$('html, body').animate({scrollTop: $(where_to).offset().top - 20}, 500); //$(window).height()*.25
+	});
+
 });
 
 
